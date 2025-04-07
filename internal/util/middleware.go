e71fb3f2a79132c5bb76755cb6d9b4228d297400
MyungJae Lee <matthewmjlee@gmail.com>
2025-04-07 01:39:13 -0500

    options

--- internal/util/middleware.go
+++ internal/util/middleware.go
@@ -61,7 +61,7 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "https://mjlee.dev")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method == http.MethodOptions {
