d10c94f02a41d30e93b8a94a244e9b3fe126a16a
MyungJae Lee <matthewmjlee@gmail.com>
2025-04-07 00:37:29 -0500

    find missing candles

--- api/candle.go
+++ api/candle.go
@@ -2,8 +2,11 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/mmjlee/btc-analysis/internal/repository"
 )
@@ -27,16 +30,43 @@ func (c CandleHandler) GetCandles(w http.ResponseWriter, r *http.Request) {
 	limit := r.URL.Query().Get("limit")
 	offset := r.URL.Query().Get("offset")
 	if ticker == "" || start == "" || end == "" || limit == "" || offset == "" {
-		log.Panic("Error: API-GetProduct: missing required query param")
+		log.Panic("Error: API-GetCandles: missing required query param")
 	}
 
 	candles, err := c.CandlePool.GetCandles(ticker, start, end, limit, offset)
 	if err != nil {
-		log.Panicf("Error: API-GetProduct-GetCandles: %v", err)
+		log.Panicf("Error: API-GetCandles-GetCandles: %v", err)
 	}
 	json_data, err := json.Marshal(candles)
 	if err != nil {
-		log.Panicf("Error: API-GetProduct-Marshal: %v", err)
+		log.Panicf("Error: API-GetCandles-Marshal: %v", err)
+	}
+	w.Write(json_data)
+}
+
+func (c CandleHandler) GetMissingCandles(w http.ResponseWriter, r *http.Request) {
+	ticker := r.PathValue("ticker")
+	start := r.URL.Query().Get("start")
+	end := r.URL.Query().Get("end")
+	limit := r.URL.Query().Get("limit")
+	offset := r.URL.Query().Get("offset")
+	if ticker == "" || start == "" || end == "" || limit == "" || offset == "" {
+		log.Panic("Error: API-GetMissingCandles: missing required query param")
+	}
+	start_int, err := strconv.ParseInt(start, 10, 64)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	start = strconv.FormatInt(time.Unix(start_int, 0).Truncate(time.Minute).Unix(), 10)
+
+	candles, err := c.CandlePool.GetMissingCandles(ticker, start, end, limit, offset)
+	if err != nil {
+		log.Panicf("Error: API-GetMissingCandles-GetCandles: %v", err)
+	}
+	json_data, err := json.Marshal(candles)
+	if err != nil {
+		log.Panicf("Error: API-GetMissingCandles-Marshal: %v", err)
 	}
 	w.Write(json_data)
 }
